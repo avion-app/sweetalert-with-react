8ce51180c48c5aea662e82c6274365da5261f05a
Tim Ramage <tjramage@gmail.com>
2024-07-29 18:14:27 +0100

    Tests upgrade for React v18 support

--- src/sweetalert.js
+++ src/sweetalert.js
@@ -1,32 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { useEffect } from 'react'
+import { createRoot } from 'react-dom/client'
+import transformer, { bindActions } from '@sweetalert/transformer'
 
-import transformer, { bindActions } from '@sweetalert/transformer';
+const CallbackWrapper = ({ callback, children }) => {
+  useEffect(() => { callback() }, [callback])
+
+  return children
+}
 
 /*
  * Convert <Element /> to pure DOM node using ReactDOM
  * (remember that ReactDOM.render() is async!)
  */
 const getDOMNodeFromJSX = (Element) => {
-  const wrapper = document.createElement('div');
+  const wrapper = document.createElement('div')
+  const root = createRoot(wrapper)
 
   return new Promise((resolve) => {
-    ReactDOM.render(Element, wrapper, () => {
-      const el = wrapper.firstChild;
+    const callback = () => resolve(wrapper.firstChild)
 
-      return resolve(el);
-    });
-  });
-};
+    const WrappedElement = () => (
+      <CallbackWrapper callback={callback}>
+        <Element />
+      </CallbackWrapper>
+    )
+
+    root.render(WrappedElement)
+  })
+}
 
 const swal = (...params) => (
   transformer(params, {
     identifier: React.isValidElement,
     transformer: getDOMNodeFromJSX, 
   })
-);
+)
 
-bindActions(swal);
+bindActions(swal)
 
-export default swal;
+export default swal
 
